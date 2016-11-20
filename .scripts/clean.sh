6ff76e215c16be12c7fea39d068266f08feac6b3
NoahVlncrt <noah.vaillancourt@gmail.com>
2016-11-20 14:44:58 -0500

    removed uneeded lines

--- .scripts/clean.sh
+++ .scripts/clean.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 echo "Cleaning up uneeded files!"
 
-#.md file variables to delete
-
-SERVER=./server/server.md
-PAGES=./imports/ui/pages/pages.md
-SERVER2=./imports/startup/server/server.md
-API=./imports/api/api.md
 
 # start deletion
-rm $SERVER
-rm $PAGES
-rm $SERVER2
-rm $API
-rm ReadMe.md
+
+rm readme.md
 rm -rf .git
 
 # deletion finished
